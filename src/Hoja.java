ba251a97e7f8bd8b46bcdcf28475c1229754b1e1
Usuario <Usuario@192.168.0.11>
2016-11-02 01:24:38 -0500

    foodCo

---
+++ src/Hoja.java
@@ -0,0 +1,24 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cesar velasco
+ */
+public abstract class Hoja {
+    
+ //   public Date fecha;
+  //  public int numeroTrabajo;
+   // public int linea;
+  public abstract int getNumTrabajo();
+  public abstract int getLinea();
+  public abstract String getFecha();
+  
+    
+}
